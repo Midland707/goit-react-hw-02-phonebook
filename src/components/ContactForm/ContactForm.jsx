28bb77cd726661ad94b813a73918ba86b7bda1da
Midland707 <rus14job@ukr.net>
2023-02-28 00:24:44 +0200

    commit PropTypes

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import css from './ContactForm.module.css';
 
 export const ContactForm = ({ onSubmitForm, onChangeName }) => (
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import css from './ContactList.module.css';
 
 export const ContactList = ({ contacts }) => (
